2bcad04d3fca2f4e1da8177a1e29ade0012545d4
peyton <peyplaisance@gmail.com>
2025-06-25 15:40:33 -0600

    cont

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@hey-api/openapi-ts'
 
 export default defineConfig({
   client: '@hey-api/client-fetch',
-  input: 'https://api-dao.in8.com/swagger/json',
+  input: 'https://api-dao.in8.com/swagger/json', // TODO: change to the correct url
   output: {
     format: 'prettier',
     path: './src/client',
