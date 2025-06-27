d90fcd974b1968e10d3b0feeac61416450d4da6d
peyton <peyplaisance@gmail.com>
2025-06-27 10:21:24 -0600

    removing dead code

--- src/components/common/Providers.tsx
+++ src/components/common/Providers.tsx
@@ -5,16 +5,11 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit'
 import React from 'react'
 import '@rainbow-me/rainbowkit/styles.css'
-import { client } from '@/src/client'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [mounted, setMounted] = React.useState(false)
   const [queryClient] = React.useState<QueryClient>(() => new QueryClient())
   React.useEffect(() => setMounted(true), [])
-
-  client.setConfig({
-    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || ''
-  })
 
   if (!mounted) return null
 
