b7559af092c4fda4f8be7f9e4918a3eede9828fc
peyton <peyplaisance@gmail.com>
2025-07-30 14:23:57 -0600

    format

--- src/hooks/types.ts
+++ src/hooks/types.ts
@@ -19,7 +19,10 @@ export interface UseLoansReturn {
 
   // Operations from useLoanOperations
   createLoan: (loanRequest: LoanRequest) => Promise<`0x${string}` | undefined>
-  extendLoan: (loanId: `0x${string}`, maxExtension: bigint) => Promise<`0x${string}` | undefined>
+  extendLoan: (
+    loanId: `0x${string}`,
+    maxExtension: bigint
+  ) => Promise<`0x${string}` | undefined>
   approveLoanFee: (feeAmount?: bigint) => Promise<`0x${string}` | undefined>
   payLoan: (
     loanId: `0x${string}`,
